72a0f048c26e43b137ce391ff3d16ef742a302f7
Deyvid Beltran <deyvid44@hotmail.com>
2024-12-02 18:34:18 -0500

    Part 1: Exercises 1.6-1.11

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,67 +1,25 @@
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
-  )
-}
+import { useState } from "react"
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  )
-}
+const Display = (props) => <div>{props.value}</div>
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const total =
-    props.parts[0].exercises +
-    props.parts[1].exercises +
-    props.parts[2].exercises
-  return (
-    <div>
-      <p>Number of exercises {total}</p>
-    </div>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+)
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => {
+    console.log("value now", newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react"
+
+const StatisticsLine = (props) => {
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
+
+const Statistics = (props) => {
+  if (props.good > 0 || props.neutral > 0 || props.bad > 0) {
+    return (
+      <table>
+        <StatisticsLine text="good" value={props.good} />
+        <StatisticsLine text="neutral" value={props.neutral} />
+        <StatisticsLine text="bad" value={props.bad} />
+        <StatisticsLine
+          text="all"
+          value={props.good + props.neutral + props.bad}
+        />
+        <StatisticsLine
+          text="average"
+          value={
+            (props.good - props.bad) / (props.good + props.neutral + props.bad)
+          }
+        />
+        <StatisticsLine
+          text="positive"
+          value={
+            (props.good / (props.good + props.neutral + props.bad)) * 100 + "%"
+          }
+        />
+      </table>
+    )
+  } else {
+    return <p>No feedback given</p>
+  }
+}
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>
+}
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Unicafe</h1>
+      <h2>Give feedback</h2>
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+export default App
