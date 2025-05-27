59efffcf45f9584072d26bb4920f7e48bb7c67eb
Deyvid Beltran <deyvid44@hotmail.com>
2025-05-27 15:28:37 -0500

    Part 2: Exercises 2.3-2.5

---
+++ part2/colections/src/components/Course.jsx
@@ -0,0 +1,33 @@
+const Header = ({ course }) => <h1>{course}</h1>
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({ parts }) => (
+  <b>
+    Total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
+  </b>
+)
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default Course
